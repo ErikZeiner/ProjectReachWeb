763a77619a6faf5c7d53e43b2bf08e5145cb487a
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2025-04-15 21:45:07 +0200

    finalise experiment design

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <Experiment title="magpie demo">
     <InstructionScreen :title="'Welcome'">
-      <p>Thank you for participating in this experiment. It should take you no more than about X min to complete it.</p>
+      <p>Thank you for participating in the experiment. It should take you no more than about 10 min to complete.</p>
       
-      <p>Please make sure to wear headphones and be in a quiet environment without distractions. You need to be able to watch short videos with audio.</p>
+      <p>Please make sure to wear headphones and be in a quiet environment without distractions – you need to be able to watch short videos with audio.</p>
 
       <p>Click on the button below to receive instructions.</p>
     </InstructionScreen>
 
     <InstructionScreen :title="'Instructions'">
-      <p>You will see a series of short video clips. In these clips you will see a speaker uttering a sentence. The speaker will be framed from below the neck to the waist.</p>
-      <p>Your task is to watch the clips (each will play twice) and answer accompanying questions. Use the slider to ??????????? </p>
+      <p> You will be watching a series of short video clips of a speaker uttering simple sentences. The speaker will be framed form below the neck to the waist and you will not see their face.</p>
+      <p>Your task is to watch the clips (each will play twice) and answer accompanying questions using a slider marked ‘no’ at one end and ‘yes’ at the other, </p>
       <p>First, you can practice on two clips. </p>
     </InstructionScreen>
     
-    <template v-for="(trial, i) of training_trials">
+    <template v-for="(trial, i) of trainingTrials">
       <SliderScreen
         :key = "i"
         :question="'Is the speaker asking whether ' + trial.content + '?'"
@@ -22,16 +22,22 @@
         optionLeft="no"
         optionRight="yes">
       <template #stimulus>
-          <video :src="trial.clip" autoplay/>
+          <video :src="`/video/training/${trial.target}_${trial.variant}.mp4`" autoplay/>
+          <Record :data="{
+                        target: trial.target,
+                        variant: trial.variant,
+                      }" />
+          
       </template>
+      
       </SliderScreen>
     </template>
     
     <InstructionScreen :title="'Instructions'">
-    <p> Lovely! Now you are ready to start the experiment proper.</p>
+    <p> Lovely! Press next to start the experiment proper.</p>
     </InstructionScreen>
 
-    <template v-for="(trial, i) of main_trials">
+    <template v-for="(trial, i) of assignedTrials">
       <SliderScreen
         :key = "i"
         :question="'Is the speaker asking whether ' + trial.content + '?'"
@@ -39,7 +45,12 @@
         optionLeft="no"
         optionRight="yes">
       <template #stimulus>
-          <video :src="trial.clip" autoplay/>
+          <video :src="`/video/main/${trial.target}_${trial.variant}_${trial.beat}.mp4`" autoplay/>
+          <Record :data="{
+                        target: trial.target,
+                        variant: trial.variant,
+                        beat: trial.beat,
+                      }" />
       </template>
       </SliderScreen>
     </template>
@@ -52,8 +63,8 @@
 </template>
 
 <script>
-import main_trials from '../trials/main_trials.csv'
-import training_trials from '../trials/training_trials.csv'
+import mainTrials from '../trials/mainTrials.csv'
+import trainingTrials from '../trials/trainingTrials.csv'
 import _ from 'lodash'
 import { ref } from 'vue'
 
@@ -61,9 +72,56 @@ export default {
   name: 'App',
   data() {
     return {
-      main_trials: _.shuffle(main_trials),
-      training_trials: _.shuffle(training_trials),
+      mainTrials: mainTrials,
+      trainingTrials: _.shuffle(trainingTrials),
+      participantIndex:0
     }
-  }
+  },
+  mounted() {
+    const id = this.$root.magpie?.participant_id || "anon0";
+    this.participantIndex = parseInt(id.match(/\d+/)?.[0]) || 0;
+  },
+  methods: {
+    shuffleArray(array) {
+      const shuffled = [...array];
+      for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+      }
+      return shuffled;
+    }
+  },
+  computed: {
+      assignedTrials() {
+        // Group by target
+        const grouped = {};
+        this.mainTrials.forEach(trial => {
+          if (!grouped[trial.target]) {
+            grouped[trial.target] = [];
+          }
+          grouped[trial.target].push(trial);
+        });
+        
+        const assigned = [];
+        
+        Object.entries(grouped).forEach(([target, variants], i) => {
+          // Sort so A is index 0, B is index 1
+          variants.sort((a, b) => a.variant.localeCompare(b.variant));
+          
+          // Decide which variant gets the video
+          const videoIndex = (this.participantIndex + i) % 2;
+          
+          variants.forEach((trial, j) => {
+            assigned.push({
+              ...trial,
+              beat: j === videoIndex ? 1 : 0
+            });
+          });
+        });
+        
+        // Shuffle full trial list
+        return this.shuffleArray(assigned);
+      }
+    }
 }
 </script>
